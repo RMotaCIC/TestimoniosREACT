4517508a6cfed7c51cff1a425bff356e2f3424ba
Randy <randymotaj@gmail.com>
2026-05-22 06:45:04 -0400

    Modificaciones y pruebas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { users } from "./users/users.js";
 function App() {
   return (
     <div className="App">
+
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos acerca de freeCodeCamp</h1>
         <Testimonio 
@@ -31,6 +32,7 @@ function App() {
           empresa= {users[2].empresa}
           testimonio= { <>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify."</> } />  
       </div>
+      
     </div>
   );
 }
