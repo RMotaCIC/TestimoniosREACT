4c2c408593866f224b8e5045954ec56cf49dbaa8
Randy <randymotaj@gmail.com>
2026-05-25 19:50:01 -0400

    Probando componentes de clase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Testimonio from "./components/Testimonio.js";
+import TestimonioClase from './components/TestimonioClase.js';
 import { users } from "./users/users.js";
 
 function App() {
@@ -8,29 +9,29 @@ function App() {
 
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos acerca de freeCodeCamp</h1>
-        <Testimonio 
+        <TestimonioClase 
           imagen= {users[0].imagen}
           nombre= {users[0].nombre}
           pais= {users[0].pais}
           cargo= {users[0].cargo}
           empresa= {users[0].empresa}
-          testimonio= { <>"It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life.</strong>" </> } />
+          testimonio= { <>It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life.</strong> </> } />
 
-        <Testimonio 
+        <TestimonioClase 
           imagen= {users[1].imagen}
           nombre= {users[1].nombre}
           pais= {users[1].pais}
           cargo= {users[1].cargo}
           empresa= {users[1].empresa}
-          testimonio= { <>"<strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."</> } />
+          testimonio= { <><strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</> } />
 
-        <Testimonio 
+        <TestimonioClase 
           imagen= {users[2].imagen}
           nombre= {users[2].nombre}
           pais= {users[2].pais}
           cargo= {users[2].cargo}
           empresa= {users[2].empresa}
-          testimonio= { <>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify."</> } />  
+          testimonio= { <>I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify.</> } />  
       </div>
       
     </div>
